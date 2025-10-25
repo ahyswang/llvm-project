3e4fafa30b309b78a3f34905b2a825a17be89871
ahyswang <ahyswang@126.com>
2025-10-25 16:19:25 +0800

    [add] add build.sh

--- demo/lln-mlir/scripts/build.sh
+++ demo/lln-mlir/scripts/build.sh
@@ -3,8 +3,7 @@
 PREFIX=$PWD/../../build_debug
 BUILD_DIR=$PWD/../../build_debug
 
-cmake -B./build \ 
--H./ \
+cmake -B./build -H./ \
 -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
 -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
 -DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit \
